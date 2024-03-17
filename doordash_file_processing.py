30a5beb8c5ffb802e8261be3854e226babfb84ba
Rajaram Rathinam <rajarambca@gmail.com>
2024-03-17 20:08:46 +0530

    Modified doordash file

--- doordash_file_processing.py
+++ doordash_file_processing.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import boto3
+import io
+import datetime
 
 s3_client = boto3.client('s3')
 sns_client = boto3.client('sns')
 sns_arn='arn:aws:sns:us-west-1:709114567321:doordash_delivery_notification'
-
+target_s3='doordash-target-zon'
 
 def lambda_handler(event, context):
 
@@ -16,12 +18,17 @@ def lambda_handler(event, context):
         #Getting s3 filename
         s3_file = event['Records'][0]['s3']['object']['key'] 
         #Reading the s3 file using bucket and key
-        resp = s3_client.get_object(Bucket=bucket, Key=s3_file)
-        
+        resp = s3_client.get_object(Bucket=bucket, Key=s3_file)        
         #Reading the file content as json
-        json_Df = pd.read_json(resp['Body'])
-        print(json_Df)
-        
+        json_Df = pd.read_json(resp['Body'])       
+        #Filetering the records with status as delivered
+        result_df=json_Df[json_Df['status'] == 'delivered']
+        #writing the filetered df as json file into s3 bucket
+        json_buffer = io.StringIO()
+        result_df.to_json(json_buffer)        
+        tgt_bucket = s3_client.Bucket('doordash-target-zon')
+        destination = "output_" + str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.json'
+        tgt_bucket.put_object(Key=destination, Body=json_buffer.getvalue())
         #sending success alert mail
         message = "DoorDash {} file has been processed successfully !!".format("s3://"+bucket+"/"+s3_file)
         success_alert = sns_client.publish(Subject="SUCCESS - Daily data processing",TargetArn=sns_arn,Message=message,MessageStructure='text')
