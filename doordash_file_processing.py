4e9f27b7b0dbc50627243d759219c02f8c20947a
Rajaram Rathinam <rajarambca@gmail.com>
2024-03-18 23:46:06 +0530

    final change

--- doordash_file_processing.py
+++ doordash_file_processing.py
@@ -36,8 +36,9 @@ def lambda_handler(event, context):
         print(result_df)
         #writing the filetered df as json file into s3 bucket
         df1=result_df.to_json()
-        target_file = s3_file[:16]
-        s3_client.put_object(Body=df1, Bucket=target_s3,Key=target_file.json)
+        target = s3_file[:10]
+        target_file =target+"_output.json"
+        s3_client.put_object(Body=df1, Bucket=target_s3,Key=target_file)
         #sending success alert mail
         message = "DoorDash {} file has been processed successfully !!".format("s3://"+bucket+"/"+s3_file)
         success_alert = sns_client.publish(Subject="SUCCESS - Daily data processing",TargetArn=sns_arn,Message=message,MessageStructure='text')
