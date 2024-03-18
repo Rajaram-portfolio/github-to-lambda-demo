e7d3dd43642f148ffbd5e5b344ad7dd95e92741d
Rajaram Rathinam <rajarambca@gmail.com>
2024-03-18 22:51:57 +0530

    final change

--- doordash_file_processing.py
+++ doordash_file_processing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import boto3
-import io
-import datetime
+from io import StringIO
+
 
 s3_client = boto3.client('s3')
 sns_client = boto3.client('sns')
@@ -10,28 +10,33 @@ target_s3='doordash-target-zon'
 
 def lambda_handler(event, context):
 
-    print(event)
+    #print(event)
 
     try:
         #Getting bucket name
         bucket  = event['Records'][0]['s3']['bucket']['name']
-        print(bucket)
+        #print(bucket)
+        
         #Getting s3 filename
         s3_file = event['Records'][0]['s3']['object']['key'] 
-        print(s3_file)
+        #print(s3_file)
+        
         #Reading the s3 file using bucket and key
-        resp = s3_client.get_object(Bucket=bucket, Key=s3_file)        
+        resp = s3_client.get_object(Bucket=bucket, Key=s3_file) 
+        
         #Reading the file content as json
-        df = pd.read_csv(resp['Body'])       
+        file_content = resp["Body"].read().decode('utf-8')
+        
+        # Read the content using pandas
+        df = pd.read_json(StringIO(file_content),lines=True)
+        #print(df)
+        
         #Filetering the records with status as delivered
-        result_df=df['status'] = 'delivered'
+        result_df=df[df['status'] == 'delivered']
+        
         #writing the filetered df as json file into s3 bucket
-        print(result_df)
-        # json_buffer = io.StringIO()
-        # result_df.to_json(json_buffer)        
-        # tgt_bucket = s3_client.Bucket('doordash-target-zon')
-        # destination = "output_" + str(datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.json'
-        # tgt_bucket.put_object(Key=destination, Body=json_buffer.getvalue())
+        df1=result_df.to_json()
+        s3_client.put_object(Body=df1, Bucket=target_s3,Key='output.json')
         #sending success alert mail
         message = "DoorDash {} file has been processed successfully !!".format("s3://"+bucket+"/"+s3_file)
         success_alert = sns_client.publish(Subject="SUCCESS - Daily data processing",TargetArn=sns_arn,Message=message,MessageStructure='text')
